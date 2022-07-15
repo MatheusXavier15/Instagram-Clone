f76119aa5c1c10cd0c1ca8f61e3c12901e42ae96
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2022-07-15 01:22:31 -0300

    Initial Commit

---
+++ Instagram-Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram-Clone
+//
+//  Created by Matheus Xavier on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
