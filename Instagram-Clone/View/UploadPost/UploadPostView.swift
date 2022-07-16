9d1af155d0736338f12446480edc62763d2af508
Matheus Xavier <matheusxavier055@gmail.com>
2022-07-16 02:22:00 -0300

    [Feature] Image Picker + Upload View

--- Instagram-Clone/View/UploadPost/UploadPostView.swift
+++ Instagram-Clone/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var text: String = ""
+    @State var imagePickerPresented = false
     var body: some View {
-        Text("Upload post view")
+        VStack {
+            if postImage == nil {
+                Button {
+                    self.imagePickerPresented.toggle()
+                } label: {
+                    VStack {
+                        Image(systemName: "plus.circle")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 140, height: 140)
+                            .foregroundColor(.black)
+                        Text("Photo")
+                            .foregroundColor(.black)
+                            .font(.system(size: 24, weight: .semibold))
+                    }.padding(.top, 36)
+                }.sheet(isPresented: $imagePickerPresented, onDismiss: {
+                    guard let selectedImage = selectedImage else { return }
+                    postImage = Image(uiImage: selectedImage)
+                }) {
+                    ImagePicker(image: $selectedImage)
+                }
+            } else if let postImage = postImage {
+                HStack(alignment: .top) {
+                    postImage
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption...", text: $text)
+                }.padding()
+                Button {
+                    //
+                } label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }.padding()
+
+            }
+            Spacer()
+        }
+        
     }
 }
 
